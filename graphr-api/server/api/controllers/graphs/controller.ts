fb66d76f5fd9b0a4ec7283f734facb20bc3dfd75
Drew Cunningham <atacunningham@users.noreply.github.com>
2020-05-23 00:26:34 +0100

    Updated YAML routes to match API

--- graphr-api/server/api/controllers/graphs/controller.ts
+++ graphr-api/server/api/controllers/graphs/controller.ts
@@ -3,26 +3,27 @@ import { IGraphService, GraphService } from '../../services/graph.service';
 import { AccountRepository } from '../../services/accountrepository.service';
 import { AdviserRepository, Adviser } from '../../services/adviserrepository.service';
 import { FirmRepository } from '../../services/firmrepository.service';
+import logger from '../../../common/logger'
 
 export class Controller { 
-    constructor(private graphService: IGraphService) { }
+    //constructor(private graphService: IGraphService) { }
 
     getGraph(req: Request, res: Response): void { 
         const id = Number.parseInt(req.params['id']);
-        console.log(id);
-        this.graphService.getAccountGraph(id)
-            .then(graph => {
-              console.log(graph);
-              if(graph) res.json(graph.toGraphModel())
-              else res.status(404).end()
-            });
+        logger.info(`${id}`);
+        // this.graphService.getAccountGraph(id)
+        //     .then(graph => {
+        //       console.log(graph);
+        //       if(graph) res.json(graph.toGraphModel())
+        //       else res.status(404).end()
+        //     });
     }
 }
-
+    
 export default new Controller(
-    new GraphService(
-        new AccountRepository(),
-        new AdviserRepository(),
-        new FirmRepository()
-    )
+    // new GraphService(
+    //     new AccountRepository(),
+    //     new AdviserRepository(),
+    //     new FirmRepository()
+    // )
 );
